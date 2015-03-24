9949d260897f8a2239d443a0fcb73df7664cc6ca
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-24 11:39:17 -0500

    Adds DbInit

---
+++ load-board-api/Persistence/DbInit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace load_board_api.Persistence
+{
+    public class DbInit : CreateDatabaseIfNotExists<LoadBoardDbContext>
+    {
+        protected override void Seed(LoadBoardDbContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
