665a89650ef8758ef8e5b04e07f30db19d8e20d9
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-24 10:37:37 -0500

    Fixes build errors

--- load-board-api/Global.asax.cs
+++ load-board-api/Global.asax.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace load_board_api
@@ -13,10 +11,7 @@ namespace load_board_api
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
