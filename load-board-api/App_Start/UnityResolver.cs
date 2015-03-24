7344ac301e072e24694e8710b3a74ed39beade5b
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-24 13:08:02 -0500

    Adds UnityResolver

---
+++ load-board-api/App_Start/UnityResolver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.Unity;
+using System.Web.Http.Dependencies;
+
+namespace load_board_api.App_Start
+{
+    public class UnityResolver : IDependencyResolver
+    {
+        protected IUnityContainer Container;
+
+        public UnityResolver(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            this.Container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return Container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return Container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = Container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        public void Dispose()
+        {
+            Container.Dispose();
+        }
+    }
+}
