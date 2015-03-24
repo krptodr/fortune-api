05cacbba5e73a64b65d05a07a1617e81485a7cd3
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-24 10:40:26 -0500

    Fixes values controller

--- load-board-api/App_Start/WebApiConfig.cs
+++ load-board-api/App_Start/WebApiConfig.cs
@@ -11,10 +11,6 @@ namespace load_board_api
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-            // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- load-board-api/Controllers/ValuesController.cs
+++ load-board-api/Controllers/ValuesController.cs
@@ -7,7 +7,6 @@ using System.Web.Http;
 
 namespace load_board_api.Controllers
 {
-    [Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
