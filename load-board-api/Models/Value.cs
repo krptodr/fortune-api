26aac271d09836c8c5c7f2b48792ee45bee4e9e0
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-24 11:45:02 -0500

    Adds test class Value

---
+++ load-board-api/Models/Value.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace load_board_api.Models
+{
+    public class Value
+    {
+        [Key]
+        public Guid Id;
+        [Required]
+        public string Name;
+    }
+}
--- load-board-api/Persistence/LoadBoardDbContext.cs
+++ load-board-api/Persistence/LoadBoardDbContext.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using load_board_api.Models;
 
 namespace load_board_api.Persistence
 {
     public class LoadBoardDbContext : DbContext
     {
+        public DbSet<Value> Values { get; set; }
+
         public LoadBoardDbContext()
             : base("LoadBoardDbContext")
         {
-
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
