2e5c49ab50430fffc60c146adf206a3c80991354
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-24 13:29:38 -0500

    Adds IUnitOfWork

---
+++ load-board-api/Persistence/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using load_board_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace load_board_api.Persistence
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IRepo<Value> ValueRepo { get; set; }
+
+        void Save();
+    }
+}
