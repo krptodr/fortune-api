54d3b2c539d4ce84357ff688a95995c9a6443e8c
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-24 11:34:00 -0500

    Adds LoadBoardDbContext and Entity Framework dependency

---
+++ load-board-api/Persistence/LoadBoardDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace load_board_api.Persistence
+{
+    public class LoadBoardDbContext : DbContext
+    {
+        public LoadBoardDbContext()
+            : base("LoadBoardDbContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
