c925d98d8df62c6773d1c7cc2b2253888c181c8b
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-26 17:20:57 -0500

    Adds Trailer

---
+++ load-board-api/Models/Trailer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace load_board_api.Models
+{
+    public class Trailer
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid LocationId { get; set; }
+        [ForeignKey("LocationId")]
+        public virtual Location Location { get; set; }
+        [Required, DefaultValue(false)]
+        public bool Deleted { get; set; }
+        [Required]
+        public DateTime LastUpdated { get; set; }
+    }
+}
