025283fcb176141fd3e6aee0b8be663ae26a17af
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-26 17:23:46 -0500

    Adds TrailerDto

---
+++ load-board-api/Dtos/TrailerDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace load_board_api.Dtos
+{
+    public class TrailerDto
+    {
+        public Guid Id { get; set; }
+        public LocationDto Location { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
