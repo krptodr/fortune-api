2cf6b2261ac1b04711800e9ef390c1d4f05b74f7
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-26 09:14:22 -0500

    Adds LocationDto

---
+++ load-board-api/LocationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace load_board_api
+{
+    public class LocationDto
+    {
+        public Guid Id;
+        public string Name;
+        public bool Deleted;
+        public DateTime LastUpdated;
+    }
+}
