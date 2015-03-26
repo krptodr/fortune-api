2caf7fe73817241499c20d9363eb58905ff2b1a2
Ryan Helmoski <ryanhelmoski@gmail.com>
2015-03-26 09:12:53 -0500

    Adds Location

---
+++ load-board-api/Models/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace load_board_api.Models
+{
+    public class Location
+    {
+        [Key]
+        public Guid Id;
+        [Required]
+        public string Name;
+        [Required]
+        public bool Deleted;
+        [Required, Column(TypeName = "datetime2")]
+        public DateTime LastUpdated;
+    }
+}
